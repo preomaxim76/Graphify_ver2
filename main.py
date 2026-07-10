233bd7bda9e5178a77b7c6c104f18b537cca130d
preomaxim <preomaxim76@gmail.com>
2026-07-10 08:45:17 +0400

    Added tokens_are_valid() function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from os import system, name
 import math
+import sys
 
 # Clear terminal
 def clear() -> None:
@@ -15,10 +16,11 @@ def clear() -> None:
 
 valid_tokens = {
     "number": ["operator", "number", ")", "pro_operator", "var", "(", ""], 
-    "operator": ["number", "(", "var"],
+    "operator": ["number", "(", "var", "pro_operator"],
     "(": ["number", "(", "pro_operator", "var"],
-    ")": ["number", "(", "operator", ""],
+    ")": ["number", "(", "operator", ")", ""],
     "pro_operator": ["number", "("],
+    "var": ["operator", "pro_operator", "(", ")", ""]
 }
 
 operators = {
@@ -28,9 +30,11 @@ operators = {
 
 # Input
 def get_equation() -> str:
-    statement = "Please enter your equation: "
+    statement = "Please enter your equation (quit to stop): "
     while True:
         eq = input(statement).lower()
+        if eq == "quit":
+            sys.exit()
         func_call = equation_is_valid(eq)
         if func_call[0]:
             return eq
@@ -51,8 +55,10 @@ def equation_is_valid(eq: str) -> tuple[bool, str]:
         return False, "Error: 'y' or 'f(x)' has not been found..."
 
     # Second half check: surface_level
-    if second_half.isspace():
+    if second_half.isspace() or second_half == "":
         return False, "Error: no equation has been found..."
+    if second_half.count('(') != second_half.count(')'):
+        return False, "Brackets don't match"
 
     return True, "No error has been detected"
 
@@ -77,10 +83,62 @@ def tokenize(half: str) -> list[str]:
     
     return stack
 
-
 # Check if the second part is valid
-def tokens_are_valid() -> tuple[bool, str]:
-    pass
+def tokens_are_valid(tokens: list[str]) -> tuple[bool, str, list[str]]:
+    possible = operators["operator"] + operators["pro_operator"] + [")", "("]
+
+    last_token = ""
+    new_token = ""
+    var = ""
+    for token in tokens:
+        if not (token.isdigit() or token.isalpha() or token in possible):
+            return False, f"{token} is not registered"
+        if token == "y":
+            return False, "'y' can not be used as a variable"
+        
+        if not last_token and token in ("+", "*", "/"):
+            return False, "Can't start with an operator"
+
+        if token.isdigit():
+            new_token = "number"
+        elif token.isalpha():
+            if len(token) > 4:
+                return False, "Too many characters given for one word"
+            if len(token) == 1:
+                if not var:
+                    var = token
+                    new_token = "var"
+                elif token == var:
+                    new_token = "var"
+                else:
+                    return False, "Unregistered Token"
+            elif token in operators["operator"]:
+                new_token = "operator"
+            elif token in operators["pro_operator"]:
+                new_token = "pro_operator"
+            else:
+                return False, "Unregistered Character"
+
+        elif token in "()":
+            new_token = token
+        elif token in operators["operator"]:
+            new_token = "operator"
+        else:
+            new_token = "pro_operator"
+
+        if last_token:
+            for vt in valid_tokens:
+                if vt == last_token and new_token in valid_tokens[vt]:
+                    break
+            else:
+                return False, "Incorrect Sequence"
+        
+        last_token = new_token
+
+    if "" in valid_tokens[new_token]:
+        return True, "200"
+    else:
+        return False, "Cannot end with that character"
 
 # Create graph points
 def create_points():
@@ -106,12 +164,16 @@ def main():
         equation = get_equation()
 
         # Check on a deep level
-        second_half = equation.split()[1].strip()
+        second_half = equation.split("=")[1].strip()
         tokenized_half = tokenize(second_half)
 
-        if not tokens_are_valid(tokenized_half):
-            # TODO: Print error
+        func_return = tokens_are_valid(tokenized_half)
+
+        if not func_return[0]:
+            print(f"Error: {func_return[1]}")
             continue 
+        else:
+            print("checked!")
 
         
 
