350566ad5fd16314cc8aa971f73ce591f11f918e
preomaxim <preomaxim76@gmail.com>
2026-07-20 10:26:52 +0400

    Fixed Bugs, Added functionality

--- main.py
+++ main.py
@@ -21,7 +21,9 @@ valid_tokens = {
     "(": ["number", "(", "pro_operator", "var"],
     ")": ["number", "(", "operator", ")", ""],
     "pro_operator": ["number", "["],
-    "var": ["operator", "pro_operator", "(", ")", "]", ""]
+    "var": ["operator", "pro_operator", "(", ")", "]", ""],
+    "[": ["number", "pro_operator", "(", "var"],
+    "]": ["operator", "(", ")", ""]
 }
 
 operators = {
@@ -86,7 +88,7 @@ def tokenize(half: str) -> list[str]:
 
 # Check if the second part is valid
 def tokens_are_valid(tokens: list[str]) -> tuple[bool, str, list[str]]:
-    possible = operators["operator"] + operators["pro_operator"] + [")", "("]
+    possible = operators["operator"] + operators["pro_operator"] + [")", "(", "[", "]"] 
 
     last_token = ""
     new_token = ""
@@ -120,19 +122,15 @@ def tokens_are_valid(tokens: list[str]) -> tuple[bool, str, list[str]]:
             else:
                 return False, "Unregistered Character"
 
-        elif token in "()":
+        elif token in "()" or token in "[]":
             new_token = token
         elif token in operators["operator"]:
             new_token = "operator"
         else:
             new_token = "pro_operator"
 
-        if last_token:
-            for vt in valid_tokens:
-                if vt == last_token and new_token in valid_tokens[vt]:
-                    break
-            else:
-                return False, "Incorrect Sequence"
+        if last_token and new_token not in valid_tokens[last_token]:
+            return False, "Incorrect Sequence"
         
         last_token = new_token
 
@@ -145,18 +143,13 @@ def ctg(value):
     return 1 / sp.tan(value)
 
 def solve(tokens: list[str]) -> tuple[float, str]:
+    emergency_break = False
     if len(tokens) != 1:
         while len(tokens) > 1:
             # 1. Powers and Roots
             while "^" in tokens or "sqrt" in tokens:
                 for i in range(len(tokens)):
-                    if tokens[i] == "^":
-                        temp = tokens[i-1] ** tokens[i+1]
-                        del tokens[i-1:i+2]
-                        tokens.insert(i-1, temp)
-                        break
-
-                    elif tokens[i] == "sqrt":
+                    if tokens[i] == "sqrt":
                         # Square root
                         if tokens[i+1] == "[":
                             if tokens[i+2] < 0:
@@ -174,8 +167,20 @@ def solve(tokens: list[str]) -> tuple[float, str]:
                         tokens.insert(i, temp)
                         break
 
+                    elif tokens[i] == "^":
+                        if tokens[i-1] == "]" or tokens[i+1] == "]":
+                            emergency_break = True
+                            break
+                        temp = tokens[i-1] ** tokens[i+1]
+                        del tokens[i-1:i+2]
+                        tokens.insert(i-1, temp)
+                        break
+                if emergency_break:
+                    break
+
+                    
             # 2. Functions
-            if "log" in tokens or "sin" in tokens or "cos" in tokens or "tg" in tokens or "ctg" in tokens:
+            while "log" in tokens or "sin" in tokens or "cos" in tokens or "tng" in tokens or "ctg" in tokens:
                 for i in range(len(tokens)):
                     match tokens[i]:
                         case "log":
@@ -226,7 +231,7 @@ def solve(tokens: list[str]) -> tuple[float, str]:
                             tokens.insert(i, temp)
                             break
 
-                        case "tg":
+                        case "tng":
                             if tokens[i+1] == "[":
                                 radians_value = sp.rad(tokens[i+2])
                                 temp = sp.tan(radians_value).n()
@@ -251,9 +256,12 @@ def solve(tokens: list[str]) -> tuple[float, str]:
 
                             tokens.insert(i, temp)
                             break
+            if emergency_break:
+                emergency_break = False
+                continue
 
             # 3. Multiplication and division
-            if "*" in tokens or "/" in tokens:
+            while "*" in tokens or "/" in tokens:
                 for i in range(len(tokens)):
                     if tokens[i] == "*":
                         temp = tokens[i-1] * tokens[i+1]
@@ -264,13 +272,13 @@ def solve(tokens: list[str]) -> tuple[float, str]:
                     elif tokens[i] == "/":
                         if tokens[i+1] == 0:
                             return 0, "Cannot divide by 0"
-                        tokens[i-1] / tokens[i+1]
+                        temp = tokens[i-1] / tokens[i+1]
                         del tokens[i-1:i+2]
                         tokens.insert(i-1, temp)
                         break
 
             # 4. + or -
-            else:
+            while "+" in tokens or "-" in tokens:
                 for i in range(len(tokens)):
                     if tokens[i] == "+":
                         temp = tokens[i-1] + tokens[i+1]
@@ -281,40 +289,68 @@ def solve(tokens: list[str]) -> tuple[float, str]:
                     elif tokens[i] == "-":
                         tokens[i-1:i+2] = tokens[i-1] - tokens[i+1]
                         break
-    print("Solve:", tokens)
     return tokens[0]
 
 
 def evaluate(tokens: list[str]) -> float:
+    tokens = [float(i) if i.isdigit() or (i[0] == "-" and i[1:].isdigit()) or "." in i else i for i in tokens]
     while len(tokens) > 1:
         while "(" in tokens:
             start, end = len(tokens) - list(reversed(tokens)).index("(") - 1, tokens.index(")")
             tokens = tokens[0:start] + [solve(tokens[start+1:end])] + tokens[end+1:]
             print("Evaluate function: ", tokens)
             break
+        func_return = solve(tokens)
+        if type(func_return) == tuple:
+            print(func_return)
+            return
         tokens = [solve(tokens)]
         break
     return tokens[0]
 
 # Create graph points
-def create_points(tokens: list[str], var: str) -> tuple[list[float], list[float]]:
-    tokens = [float(i) if i.isdigit() else i for i in tokens]
+def create_points(tokens: list[str], var: str) -> tuple[list[int], list[float]]:
     if not var:
-        return [], [evaluate(tokens) * 100]
-    
-    return [evaluate((" ".join(tokens)).replace(var, i).split()) for i in range(-100, 101)]
+        value = evaluate(tokens)
+        return [], [value * 100]
+
+    x = []
+    for i in range(-100, 101):
+        for j in range(0, 100, 10):
+            x.append(i + round(float(f".{j}"), 1))
+
+
+    return x, [evaluate((" ".join(tokens)).replace(var, str(i)).split()) for i in x]
     
 
 # Open a window with graph
-def create_graph(tokens: list, var: str) -> None:
+def create_graph(tokens: list, var: str, equation: str) -> None:
     x_points, y_points = create_points(tokens, var)
 
-    # No variables
-    if not x_points:
-        # TODO: Build the graph
-        return
+    fig, ax = plt.subplots()
+
+    ax.spines['left'].set_position('zero')
+    ax.spines['bottom'].set_position('zero')
+
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+
+    ax.grid(True, linestyle="--")
+
+    ax.relim()
+    ax.autoscale_view()
+
+    ax.plot(1, 0, ">k", transform=ax.get_yaxis_transform(), clip_on=False)
+    ax.plot(0, 1, "^k", transform=ax.get_xaxis_transform(), clip_on=False)
     
-    # TODO: Build the graph
+    if not x_points:
+        plt.plot(y_points)
+    else:
+        plt.plot(x_points, y_points)
+
+    plt.title(f"{equation}")
+    plt.show()
+
     return
 
 
@@ -334,7 +370,7 @@ def main() -> None:
             print(f"Error: {func_return[1]}")
             continue 
         
-        create_graph(tokenized_half, func_return[2])
+        create_graph(tokenized_half, func_return[2], equation)
 
     return
 
--- test.py
+++ test.py
@@ -1,14 +1,6 @@
-from main import evaluate, tokenize, solve
+from main import create_points, tokenize
 
-tokens = tokenize("cos[30]")
-var = "x"
+tokens = tokenize("log[x]")
 
-str_tokens = "".join(tokens)
-start, end = str_tokens.rfind("("), str_tokens.find(")")
-
-tokens = [float(i) if i.isdigit() else i for i in tokens]
-
-#print(tokens)
-print(tokens)
-print(evaluate(tokens))
-#print(solve(tokens))
+func = create_points(tokens, "x")
+print(func)
